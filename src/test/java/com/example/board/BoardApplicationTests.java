0262bb2407916df809aab6e679f5268a892ec4e6
dhlee6281 <dhlee6281@raoncorp.com>
2025-01-03 17:12:11 +0900

    [BOARD] 테스트코드 수정

--- src/test/java/com/example/board/BoardApplicationTests.java
+++ src/test/java/com/example/board/BoardApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.board;
 
+import com.example.board.entity.Board;
+import com.example.board.repository.BoardRepository;
+import com.example.board.service.BoardService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class BoardApplicationTests {
 
+
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public List<Board> getBoardList(){
+        BoardRepository boardRepository = null;
+        List<Board> list =  boardRepository.searchAllByDelYnIs();
+        return list;
     }
 
 }
