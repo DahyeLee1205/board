6962a69c8149ee3738062ff87cf3b62ae12a4a58
dhlee6281 <dhlee6281@raoncorp.com>
2025-09-11 14:13:45 +0900

    [migration_Kotilin] BoardDto객체 not null 로 수정

--- src/main/kotlin/com/example/board/dto/BoardDto.kt
+++ src/main/kotlin/com/example/board/dto/BoardDto.kt
@@ -2,11 +2,11 @@ package com.example.board.dto
 
 import java.time.LocalDateTime
 
-data class BoardDto (
+data class BoardDto(
     var id: Int = 0,
-    var boardTitle : String? = "",   // 게시판 제목
-    var boardContent :  String ?= "", // 게시판 내용
-    var userNo : String ?= "",      // 사용자 번호
-    var createDate : LocalDateTime ?= LocalDateTime.now(),
-    var delYn : Int = 0
+    var boardTitle: String,   // 게시판 제목
+    var boardContent: String, // 게시판 내용
+    var userNo: String,      // 사용자 번호
+    var createDate: LocalDateTime? = LocalDateTime.now(),
+    var delYn: Int = 0
 )
